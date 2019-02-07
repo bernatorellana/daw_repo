fcef70c1c7f7f429e4942a0f52f5a9402c485fe8
Bernat <bernat.orellana@gmail.com>
2019-02-07 16:42:55 +0100

    Primera implementació

--- Primer.java
+++ Primer.java
@@ -1,4 +1,10 @@
 public class Primer {
-    public Primer() {
+    private int secret;
+    
+    public Primer(int no, int se, int que, int passa) {
+    }
+    
+    public static void main(String args[]) {
+        // Oh Oh !
     }
 }
--- Segon.java
+++ Segon.java
@@ -1,4 +1,12 @@
 public class Segon {
+    
+    private int segon;
+    
     public Segon(){
     }
+    
+    private void ticTacTicTac(){
+        
+    }
+    
 }
