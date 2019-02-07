a4a36c24000e1bd90d3b16c5f6f094f6b002eb21
BrisingrGaunt <w2.kmedina@infomila.info>
2019-02-07 16:43:05 +0100

    Faster than you

--- Primer.java
+++ Primer.java
@@ -1,3 +1,4 @@
 public class Primer {
-    
+    //hey
+    //Follow themiwui11 on Instagram he's desperate for likes!!!! 
 }
--- Segon.java
+++ Segon.java
@@ -1,3 +1,4 @@
 public class Segon {
-    
+    //@pedropuyo99 en instagram seguidlo
+    //@miguelgomez.98 seguidlo tb!!
 }
